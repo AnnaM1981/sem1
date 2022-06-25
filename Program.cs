a6ca16e9f1e38d14fccca3dc28c2714b6750e667
a.mikhalkova <ann94573220@yandex.ru>
2022-06-25 16:11:44 +0300

    Домашняя работа

--- Program.cs
+++ Program.cs
@@ -65,7 +65,75 @@
     
 // }
 // 5 Напишите программу которая на входе принимает трехзначное число и на выходе показывает последнюю цифру
-Console.Write("input N: ");
-int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(num % 10);
+// Console.Write("input N: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(num % 10);
+
+// Домашняя работа
+//Задача 1 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Console.WriteLine("input first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 > num2){
+//     Console.WriteLine ("num1 большее число, num2 меньшее число");
+// }
+
+// else
+
+// {
+//     Console.WriteLine ("num2 большее число, num1 меньшее число");
+// }
+
+// Задача 2 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("введите три числа: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
+// int c = Convert.ToInt32(Console.ReadLine());
+// if (a > b){
+//     a = max;
+// }
+// else
+// {
+// b = max;
+// }
+// if (b > c){
+//     Console.WriteLine ("max = b");
+// }
+// else 
+// {
+//     Console.WriteLine ("max = c");
+// }
+// if (c > a){
+//     Console.WriteLine ("max = c");
+// }
+
+// else  
+// {
+//     Console.WriteLine ("max = a");
+// }
+
+// Задача 3 Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// Console.WriteLine("input integer number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num % 2 == 0){
+//     Console.WriteLine("четное число");
+// }
+// else {
+//     Console.WriteLine("нечетное число");
+// }
+// Задача 4 Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// Console.Write("input N: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int current;
+// if (num % 2 == 0){
+// current = num;
+// }
+// else {
+//     current = 1 + num;
+// }
+// while (current >= 1){
+//     Console.WriteLine (current +"");
+// }
 
