24dec4b3384e915129b1938df35609bae9f595e1
a.mikhalkova <ann94573220@yandex.ru>
2022-06-28 17:28:02 +0300

    Обновление от 28 июня

--- Program.cs
+++ Program.cs
@@ -86,16 +86,16 @@
 // }
 
 // Задача 2 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-Console.WriteLine("input a");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("input b");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("input c");
-int c = Convert.ToInt32(Console.ReadLine());
-int max = a;
-if (b > max) max = b;
-if (c > max) max = c;
-Console.WriteLine (max);
+// Console.WriteLine("input a");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input b");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input c");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// Console.WriteLine (max);
 
 
 // Задача 3 Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
@@ -120,4 +120,283 @@ Console.WriteLine (max);
 // while (current >= 1){
 //     Console.WriteLine (current +"");
 // }
+
+// seminar 2
+// Задача 1. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+// void ShowNumber (){
+//     int num = new Random ().Next(10, 100);
+//     int firstNum = num / 10;
+//     int secondNum = num % 10;
+//     Console.Write(num + "->");
+//     if (firstNum > secondNum){
+//         Console.WriteLine(firstNum);
+//     }
+//     else{
+//         Console.WriteLine (secondNum);
+//     }
+// }
+
+// ShowNumber();
+
+// Задача 2. Напишите программу, которая выводит случайное 3-хзн. число и удаляет вторую цифру числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+  
+//   int NumberWithoutSecDig(int Number){
+//     int NewNumber = Number / 100 * 10 + Number % 10;
+//     return NewNumber;
+//   }
+//   int Number = new Random ().Next(100, 1000);
+//   int a =  NumberWithoutSecDig (Number);
+//   Console.WriteLine(a + " " + Number);
+
+// Задача 3. Проверить кратность второго числа к первому.
+// void MultiNum (int a, int b){
+//     if (b%a == 0){
+//         Console.WriteLine(b + " кратно " + a);
+//     }
+//     else{
+//         Console.WriteLine(b + " некратно " + a + ", остаток " + b%a);
+//     }
+// }
+// Console.Write("Введите последовательно два числа:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// MultiNum(num1, num2);
+
+// Задача 4. Написать программу, которая принимает на вход число и проверяет кратно ли оно.
+
+// void NumDiv(int a){
+//     if(a%7 == 0 && a%23 == 0){
+//         Console.WriteLine(a + " кратно 23 и 7 ");
+//     }
+//     else{
+//         Console.WriteLine(a + "не кратно 23 и 7 ");
+//     }
+// }
+// Console.Write("Введите число:");
+// NumDiv(Convert.ToInt32(Console.ReadLine()));
+
+// Семинар 3
+
+// Задача 1. Напишите программу которая принимает на вход координаты точки (х,у) причем х не равен 0, у не равен 0,
+// и выдает номер четверти плоскости, в которой находится эта точка.
+// void getDot (int x, int y) {
+//     if (x == 0 || y ==0){
+//         Console.WriteLine ("нет возможности определить четверть");
+//     }
+//     else{
+//         if (x > 0 && y > 0)
+//             Console.WriteLine ("1 четверть");
+        
+//         if (x < 0 && y > 0)
+//             Console.WriteLine ("2 четверть");
+        
+//         if (x < 0 && y < 0)
+//             Console.WriteLine ("3 четверть");
+        
+//         if (x > 0 && y < 0)
+//             Console.WriteLine ("4 четверть");
+//      }
+// }
+// Console.WriteLine ("введите координаты точки: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// int y = Convert.ToInt32(Console.ReadLine());
+// getDot (x,y);
+
+// Задача 2. Напишите программу которая по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти х и у.
+// void getDot (int num){
+//     if (num < 1 || num > 4){
+//         Console.WriteLine ("нет возможности определить диапазон координат");
+//     }
+//     else{
+//         if (num == 1)
+//             Console.WriteLine ("x > 0 && y > 0");
+        
+//         if (num == 2)
+//             Console.WriteLine ("x < 0 && y > 0");
+        
+//         if (num == 3)
+//             Console.WriteLine ("x < 0 && y < 0");
+        
+//         if (num == 4)
+//             Console.WriteLine ("x > 0 && y < 0");
+//      }
+// }
+// Console.WriteLine ("введите номер четверти: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// getDot (num);
+
+// Задача 3. Напишите программу которая принимает на вход координаты двух точек и находит пространство между ними в 2Д пространстве.
+// А (3,6); В (2,1) -> 5,09
+// A (7,-5); B (1,-1) ->7,21
+// Console.Clear();
+// double Dist(int x1, int y1, int x2, int y2){
+//     return Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2));
+// }
+// Console.WriteLine("Введите координаты х1: ");
+// int xA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты y1: ");
+// int yA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты x2: ");
+// int xB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты y2: ");
+// int yB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Длина отрезка: " + Dist(xA, yA, xB, yB));
+
+// Задача 4. Напишите программу которая принимает на вход число N и выдает таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25
+// 2 -> 1, 4
+// void listSqrt(int N){
+//     int count = 1;
+//     Console.Write (N + "-> ");
+
+//     if(N > 0)
+//     {
+
+//         while (count <= N)
+//         {
+//         Console.Write (Math.Pow(count, 2) + ", ");
+//         count ++;
+//         }
+//     }
+//     else 
+//     {
+//     count = -1;
+//     while (count >= N)
+//         {
+//         Console.Write (Math.Pow(count, 2) + ", ");
+//         count --;
+//         }
+//     }
+// }
+// Console.WriteLine ("введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// listSqrt(num);
+
+// Семинар 4
+// Задача 1. Напишите программу которая принимает на вход число (А) и выдает сумму чисел от 1 до А.
+// 7->28
+// 4->10
+// 8->36
+
+// int sumToNum(int num)
+// {
+//     int sum = 0;
+//     for (int i = 1; i <= num; i++)
+//     {
+//         sum += i;
+//     }
+//     return sum;
+// }
+// Console.WriteLine("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Sum of elements is " + sumToNum(number));
+
+// Задача 2. Напишите программу которая принимает на вход число и выдает количество цифр в числе.
+// 456->3
+// 78->2
+// 89126->5
+// int CountNumber(int num)
+// {
+//     int count = 0;
+//     while(num > 0)
+//     {
+//         num = num / 10;
+//         count ++;
+//     }
+//     return (count);
+// }
+// Console.WriteLine("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(CountNumber(number));
+
+// Задача 3. Напишите программу которая принимает на вход число N и выдает произведение чисел от 1 до N.
+
+// int MultiToNum(int num)
+// {
+//     int multiplay = 1;
+//     for (int i = 1; i <= num; i++)
+//     {
+//         multiplay *= i;
+//     }
+//     return multiplay;
+// }
+// Console.WriteLine("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Multiplay of elements is " + MultiToNum(number));
+
+// Задача 4. Напишите программу которая выводит массив из 8 элементов заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+// Создаем массив случайных чисел, где size - количество элементов в массиве, min - начало диапазона случайных чисел, max - до какого генеририруем.
+// int[] CreateRandomArray(int size){
+//     int[] array = new int[size];
+//     for(int i = 0; i < size; i++){
+//         array[i] = new Random().Next(0,2);
+//     }
+//     return array;
+// }
+// создаем массив из чисел которые вводим
+// int[] CreateArray(int size){
+
+//     int[] array = new int[size];
+//     for(int i = 0; i < size; i++){
+//         Console.Write("Input " + i + "element: ");
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return array;
+// }
+// выводим массив на экран значения массива в одну строку через пробел
+// void ShowArray(int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++){
+//         Console.Write(array[i] + " ");
+//     }
+// }
+// int[] myRandomArray = CreateRandomArray (6);
+// ShowArray(myRandomArray);
+// Console.WriteLine();
+
+// Домашняя работа (семинар 4)
+
+// Задача № 3. Напишите программу которая задает массив из 8 элементов и выводит их на экран.
+
+// int[] CreateArray(int size){
+//     int[] array = new int[size];
+//     for(int i = 0; i < size; i++){
+//         Console.Write("Input " + i + "element: ");
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return array;
+// }
+// void ShowArray(int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++){
+//         Console.Write(array[i] + " ");
+//     }
+// }
+// int[] myRandomArray = CreateArray (8);
+// ShowArray(myRandomArray);
+// Console.WriteLine();
+
+// Задача № 2. Напишите программу которая принимает на вход число и выдает сумму цифр в числе.
+
+//  int sumToNum(int num)
+// {
+//     int sum = 0;
+//     for (int i = num / 10; num > 0; i++)
+//     {
+//         sum += i;
+//     }
+//     return sum;
+// }
+// Console.WriteLine("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Sum of elements is " + sumToNum(number));
 
