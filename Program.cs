e5ae9e6d9459c11fd51ec5dc3e1294cb7fb030e6
a.mikhalkova <ann94573220@yandex.ru>
2022-07-09 12:40:12 +0300

    обновление от 09.07.2022

--- Program.cs
+++ Program.cs
@@ -738,9 +738,60 @@
 // double[,] myArray = GetArray(rows, columns, 0, 10);
 // PrintArray(myArray);
 
+// Домашняя работа (к семинару 3)
+// Задача. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
+// void listSqrt(int N){
+//     int count = 1;
+//     Console.Write (N + "-> ");
 
+//     if(N > 0)
+//     {
 
+//         while (count <= N)
+//         {
+//         Console.Write(Math.Pow(count, 3) + ", ");
+//         count ++;
+//         }
+//     }
+//     else 
+//     {
+//     count = -1;
+//     while (count >= N)
+//         {
+//         Console.Write(Math.Pow(count, 3) + ", ");
+//         count --;
+//         }
+//     }
+// }
+// Console.WriteLine ("введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// listSqrt(num);
 
+// Задача.Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-
+// int InvertedNumber(int num)
+// {
+//     int a = num % 10;
+//     int b = num / 10 % 10;
+//     int c = num / 100 % 10;
+//     int d = num / 1000 % 10;
+//     int e = num / 10000;
+//     int f = a * 10000 + b * 1000 + c * 100 + d * 10 + e;
+//     return f;
+// }
+// Console.WriteLine ("введите пятизначное число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int secondnum = InvertedNumber(number);
+// if (number > 99999 | number < 10000)
+// {
+//     Console.WriteLine ("Число не пятизначное!");
+// }
+// else if (number == secondnum)
+// {
+//     Console.WriteLine ("Число" + " " + number+ " " + "палиндром!");
+// }
+// else
+// {
+//     Console.WriteLine ("Число не является палиндромом!");
+// }
